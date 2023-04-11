daca7d38ea243fc667d2d6eab3e685b31733152d
ADIBA NOOR <adiba.noor003@gmail.com>
2023-04-11 20:13:24 +0530

    added pages part 2

---
+++ lib/pages/favpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FavPage extends StatefulWidget {
+  const FavPage({Key? key}) : super(key: key);
+
+  @override
+  State<FavPage> createState() => _FavPageState();
+}
+
+class _FavPageState extends State<FavPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
