1c5f9f002906d86fe2dafd8994991a0d80dc505c
SanskritiHarmukh <sanskritiharmukh1908@gmail.com>
2023-04-14 02:56:37 +0530

    Sign Up added

--- lib/pages/SignUppage.dart
+++ lib/pages/SignUppage.dart
@@ -1,22 +1,23 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:oppion/main.dart';
-import 'utils/auth_utils.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class Loginpage extends StatefulWidget {
-  final VoidCallback onClickedSignUp;
-  const Loginpage({Key? key, required this.onClickedSignUp}) : super(key: key);
+import '../main.dart';
+
+class SignUppage extends StatefulWidget {
+  final Function() onClickSignIn;
+  const SignUppage({Key? key, required this.onClickSignIn}) : super(key: key);
 
   @override
-  State<Loginpage> createState() => _LoginpageState();
+  State<SignUppage> createState() => _SignUppageState();
 }
 
-class _LoginpageState extends State<Loginpage> {
+class _SignUppageState extends State<SignUppage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   @override
-  void dispose(){
+  void dispose() {
     emailController.dispose();
     passwordController.dispose();
     super.dispose();
@@ -30,6 +31,12 @@ class _LoginpageState extends State<Loginpage> {
         SizedBox(height: 60),
         FlutterLogo(size: 120),
         SizedBox(height: 20),
+        Text(
+          'Hey there, \n Welcome',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 40,),
         TextField(
           controller: emailController,
           cursorColor: Colors.black,
@@ -44,40 +51,37 @@ class _LoginpageState extends State<Loginpage> {
           obscureText: true,
         ),
         SizedBox(height: 20,),
-        ElevatedButton.icon(style: ElevatedButton.styleFrom(minimumSize: Size.fromHeight(50),),onPressed: signIn, icon: Icon(Icons.lock_open, size:32), label: Text('Sign In',style:TextStyle(fontSize: 24),),),
-        SizedBox(height: 24,),
+        ElevatedButton.icon(style: ElevatedButton.styleFrom(minimumSize: Size.fromHeight(50),),onPressed: signUp, icon: Icon(Icons.arrow_forward, size:32), label: Text('Sign Up',style:TextStyle(fontSize: 24),),),
+        SizedBox(height: 20,),
         RichText(text: TextSpan(
           style: TextStyle(color: Colors.black, fontSize: 18),
-              text: 'No account? ',
-            children: [
-              TextSpan(
-                recognizer: TapGestureRecognizer()
-                  ..onTap = widget.onClickedSignUp,
-                text:'Sign Up',
-                style: TextStyle(
-                  decoration: TextDecoration.underline,
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
+          text: 'Already have an account? ',
+          children: [
+            TextSpan(
+              recognizer: TapGestureRecognizer()
+                ..onTap = widget.onClickSignIn,
+              text:'Sign In',
+              style: TextStyle(
+                decoration: TextDecoration.underline,
+                color: Theme.of(context).colorScheme.secondary,
               ),
-            ],
+            ),
+          ],
         ),),
       ],
     ),
   );
-
-  Future signIn() async {
+  Future signUp() async {
     showDialog(context: context, barrierDismissible: false,
-    builder: (context) => Center(child: CircularProgressIndicator()),);
+      builder: (context) => Center(child: CircularProgressIndicator()),);
     try {
-      await FirebaseAuth.instance.signInWithEmailAndPassword(
-          email: emailController.text.trim(),
-          password: passwordController.text.trim(),);
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),);
     } on FirebaseAuthException catch (e){
       print(e);
     }
 
     navigatorKey.currentState!.popUntil((route) => route.isFirst);
-    }
   }
-
-
+}
