a464e242b540bfe30d1ecc14df01f39c9d2be1a3
ADIBA NOOR <adiba.noor003@gmail.com>
2023-04-15 23:47:07 +0530

    done some changes

--- lib/pages/live opportunity.dart
+++ lib/pages/live opportunity.dart
@@ -14,177 +14,176 @@ class LiveOpportunity extends StatefulWidget {
 }
 
 class _LiveOpportunityState extends State<LiveOpportunity> {
-    final ref = FirebaseDatabase.instance.ref('Interships');
-    @override
-    Widget build(BuildContext context) {
-      double w = MediaQuery.of(context).size.width;
-      double h = MediaQuery.of(context).size.height;
-      final refFM = FirebaseDatabase.instance.ref('FavF_M');
-      return Scaffold(
+  final ref = FirebaseDatabase.instance.ref('Internships');
+  @override
+  Widget build(BuildContext context) {
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+    final refFM = FirebaseDatabase.instance.ref('FavF_M');
+    return Scaffold(
+      backgroundColor: Color(0xff023047),
+      appBar: AppBar(
+        leading: InkWell(
+            onTap: (){
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> CustomDrawer()));
+            },
+            child: Icon(Icons.arrow_back_ios_rounded,color: Color(0xfffb8500),)),
+        title: Text('Live Opportunities',style: TextStyle(color: Color(0xfffb8500),fontSize: 22,),),
+        elevation: 0,
         backgroundColor: Color(0xff023047),
-        appBar: AppBar(
-          leading: InkWell(
-              onTap: (){
-                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> CustomDrawer()));
-              },
-              child: Icon(Icons.arrow_back_ios_rounded,color: Color(0xfffb8500),)),
-          title: Text('Live Opportunities',style: TextStyle(color: Color(
-              0xfffb8500),fontSize: 22,),textAlign: TextAlign.start,),
-          elevation: 0,
-          backgroundColor: Color(0xff023047),
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: FirebaseAnimatedList(
-                  query: ref,
-                  itemBuilder: (context, snapshot, animation, index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 8, horizontal: 20),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                                begin: Alignment.topLeft,
-                                end: Alignment.bottomRight,
-                                colors: [
-                                  Color(0xff219ebc),
-                                  Color(0xff8ecae6),
-                                  Color(0xfff0f7f8)
-                                ]
-                            ),
-                            // borderRadius: BorderRadius.circular(20),
-                            boxShadow: [
-                              BoxShadow(color: Color(0xff8ecae6),
-                                  offset: Offset(2,2),
-                                  blurRadius: 2,
-                                  spreadRadius: 2)
-                            ],
-                            border:Border(
-                                right: BorderSide(color: Colors.white,width: 1,),
-                                bottom: BorderSide(color: Colors.white,width: 1),
-                                left: BorderSide(color:Colors.white,width: 1),
-                                top: BorderSide(color:Colors.white,width: 1)),
-                            borderRadius: BorderRadius.circular(10)),
-                        // child: ListTile(
-                        //   leading: Icon(Icons.person,color: Colors.white,),
-                        //   title: Text(snapshot.child('A').value.toString()),
-                        //   subtitle: Text(snapshot.child('B').value.toString()),
-                        //   tileColor: Color(0xFFD58DE0),
-                        //   onTap: (){
-                        //
-                        //   },
-                        //   minVerticalPadding: 10,
-                        //   contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-                        // ),
-                        child: Container(
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FirebaseAnimatedList(
+                query: ref,
+                itemBuilder: (context, snapshot, animation, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 8, horizontal: 20),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [
+                                Color(0xff219ebc),
+                                Color(0xff8ecae6),
+                                Color(0xfff0f7f8)
+                              ]
+                          ),
+                          // borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(color: Color(0xff8ecae6),
+                                offset: Offset(2,2),
+                                blurRadius: 2,
+                                spreadRadius: 2)
+                          ],
+                          border:Border(
+                              right: BorderSide(color: Colors.white,width: 1,),
+                              bottom: BorderSide(color: Colors.white,width: 1),
+                              left: BorderSide(color:Colors.white,width: 1),
+                              top: BorderSide(color:Colors.white,width: 1)),
+                          borderRadius: BorderRadius.circular(10)),
+                      // child: ListTile(
+                      //   leading: Icon(Icons.person,color: Colors.white,),
+                      //   title: Text(snapshot.child('A').value.toString()),
+                      //   subtitle: Text(snapshot.child('B').value.toString()),
+                      //   tileColor: Color(0xFFD58DE0),
+                      //   onTap: (){
+                      //
+                      //   },
+                      //   minVerticalPadding: 10,
+                      //   contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+                      // ),
+                      child:  Container(
 
-                            padding: EdgeInsets.all(10),
-                            child: Column(
-                              children: [
-                                Container(
-                                  alignment: Alignment.center,
-                                  child: Text(
-                                    snapshot.child('NAME').value.toString(),
-                                    style: TextStyle(
-                                      color: Color(0xff023047),
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.w800,
-                                    ),
-                                    textAlign: TextAlign.center,
+                          padding: EdgeInsets.all(10),
+                          child: Column(
+                            children: [
+                              Container(
+                                alignment: Alignment.center,
+                                child: Text(
+                                  snapshot.child('COMPANY NAME').value.toString(),
+                                  style: TextStyle(
+                                    color: Color(0xff023047),
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w800,
                                   ),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(15),
-                                    color: Color(0xff8ecae6),
-                                  ),
-                                  padding: EdgeInsets.symmetric(
-                                      vertical: 15, horizontal: 15),
+                                  textAlign: TextAlign.center,
                                 ),
-                                SizedBox(
-                                  height: 10,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: Color(0xff8ecae6),
                                 ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Link(
-                                      target: LinkTarget.self,
-                                      uri: Uri.parse(
-                                          "snapshot.child('LINK').value.toString()"),
-                                      // uri: Uri.parse("www.google.com"),
-                                      builder: (context, followLink) => InkWell(
-                                        onTap: () => launch(
-                                            snapshot.child('LINK').value.toString()),
-                                        child: Container(
-                                          alignment: Alignment.center,
-                                          child: Row(
-                                            mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                            children: [
-                                              Text(
-                                                'Apply Now',
-                                                style: TextStyle(
-                                                    color: Color(0xff023047),
-                                                    fontSize: 12,
-                                                    fontWeight: FontWeight.w800),
-                                              ),
-                                              Icon(
-                                                Icons.call_made_rounded,
-                                                size: 15,
-                                                color: Color(0xff219ebc),
-                                              ),
-                                            ],
-                                          ),
-                                          width: w * 0.4,
-                                          height: h * 0.05,
-                                          decoration: BoxDecoration(
-                                            color: Color(0xff8ecae6),
-                                            borderRadius: BorderRadius.circular(15),
-                                            // image: DecorationImage(
-                                            //     image: AssetImage(
-                                            //         "assets/ApplyNow.png"
-                                            //     )
-                                            // ),
-                                          ),
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 15, horizontal: 15),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Link(
+                                    target: LinkTarget.self,
+                                    uri: Uri.parse(
+                                        "snapshot.child('LINK').value.toString()"),
+                                    // uri: Uri.parse("www.google.com"),
+                                    builder: (context, followLink) => InkWell(
+                                      onTap: () => launch(
+                                          snapshot.child('LINK').value.toString()),
+                                      child: Container(
+                                        alignment: Alignment.center,
+                                        child: Row(
+                                          mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              'Apply Now',
+                                              style: TextStyle(
+                                                  color: Color(0xff023047),
+                                                  fontSize: 12,
+                                                  fontWeight: FontWeight.w800),
+                                            ),
+                                            Icon(
+                                              Icons.call_made_rounded,
+                                              size: 15,
+                                              color: Color(0xff219ebc),
+                                            ),
+                                          ],
+                                        ),
+                                        width: w * 0.4,
+                                        height: h * 0.05,
+                                        decoration: BoxDecoration(
+                                          color: Color(0xff8ecae6),
+                                          borderRadius: BorderRadius.circular(15),
+                                          // image: DecorationImage(
+                                          //     image: AssetImage(
+                                          //         "assets/ApplyNow.png"
+                                          //     )
+                                          // ),
                                         ),
                                       ),
                                     ),
-                                    SizedBox(width: 10,),
-                                    Theme(
-                                      data: ThemeData(
-                                        elevatedButtonTheme: ElevatedButtonThemeData(
-                                          style: ElevatedButton.styleFrom(
-                                              backgroundColor: Color(0xffffb703)
-                                          ),
+                                  ),
+                                  SizedBox(width: 10,),
+                                  Theme(
+                                    data: ThemeData(
+                                      elevatedButtonTheme: ElevatedButtonThemeData(
+                                        style: ElevatedButton.styleFrom(
+                                            backgroundColor: Color(0xffffb703)
                                         ),
                                       ),
-                                      child: ElevatedButton(
-                                          onPressed: () {
-                                            refFM.push().set({
-                                              "NAME": snapshot
-                                                  .child('NAME')
-                                                  .value
-                                                  .toString(),
-                                              "LINK": snapshot
-                                                  .child('LINK')
-                                                  .value
-                                                  .toString()
-                                            },
-                                            );
+                                    ),
+                                    child: ElevatedButton(
+                                        onPressed: () {
+                                          refFM.push().set({
+                                            "NAME": snapshot
+                                                .child('NAME')
+                                                .value
+                                                .toString(),
+                                            "LINK": snapshot
+                                                .child('LINK')
+                                                .value
+                                                .toString()
                                           },
-                                          child: Text("Fav",style: TextStyle(
-                                              color: Color(0xff023047),fontSize: 15),)),
-                                    ),
-                                  ],
-                                )
-                              ],
-                            )),
-                      ),
-                    );
-                  }
-              ),
-            )
-          ],
-        ),
-      );
-    }
+                                          );
+                                        },
+                                        child: Text("Fav",style: TextStyle(
+                                            color: Color(0xff023047),fontSize: 15),)),
+                                  ),
+                                ],
+                              )
+                            ],
+                          )),
+                    ),
+                  );
+                }
+            ),
+          )
+        ],
+      ),
+    );
   }
+}
